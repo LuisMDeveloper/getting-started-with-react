8b5a8e7ce3a101ea7a25724fd1fab72448374707
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2016-07-19 21:57:06 -0500

    Component state and inline styles

--- src/js/components/MessageBox.js
+++ src/js/components/MessageBox.js
@@ -3,12 +3,25 @@ import React from "react";
 import SubMessage from "./SubMessage"
 
 export default class MessageBox extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isVisible: true,
+      titleMessage: 'Hello World!'
+    }
+  }
+
   render() {
+    let inlineStyles = {
+      display: this.state.isVisible ? 'block' : 'none'
+    };
     return (
-      <div className="container">
-        <h1>Hello World!</h1>
+      <div className="container jumbotron" style={inlineStyles}>
+        <h1>{this.state.titleMessage}</h1>
         <SubMessage />
       </div>
     );
   }
+
 }
